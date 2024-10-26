81b5704c0ccd4717fa9fa964d0a5db39e19e5ea4
jamilshehab <jamilshehab>
2024-10-26 18:36:40 +0300

    changes added

--- divisiblebythreeandfive.py
+++ divisiblebythreeandfive.py
@@ -4,6 +4,13 @@ x=int(input("enter a number : "))
 #we run through the numbers by creating a loop 
 for number in range(1,x):
     #check the numbers if they are divisible by 3 and 5
-    if number % 3==0 and number % 5 == 0:    
-       #display them on the output
-       print("the numbers that are divisible by 3 and 5 are : ", number)
+    if number % 3==0:  
+       #display the number that is divisible by 3 on the output
+       print("the numbers that are divisible by 3 are : ", number)
+    if number % 5==0:      
+       #display the number that is divisible by 5 on the output
+       print("the numbers that are divisible by 5 are : ", number)
+
+ 
+ 
+   
